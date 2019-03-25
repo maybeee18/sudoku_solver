cdc1f9a5e4e4f5de5e910a9ea498e107cde6684e
Matt Gray <mgray@bbmtek.com>
2019-03-25 10:58:50 -0400

    Converted Sudoku solver functions to basic class object. Need to fix tests to use class object

--- sudokusolver/solver.py
+++ sudokusolver/solver.py
@@ -6,56 +6,58 @@ Created on Wed Mar 20 16:40:18 2019
 @author: matthewgray
 """
 
-def validInput(grid):
-    # Grid type should be 9x9 only considering list of lists for now
-    if isinstance(grid, list):
-        if len(grid) == 9:
-            if (False in [len(x) == 9 for x in grid]) == False:
-                return True
-    return False
-        
+class SudokuSolver():
 
-def findNextCellToFill(grid, i, j):
-        for x in range(i,9):
-                for y in range(j,9):
-                        if grid[x][y] == 0:
-                                return x,y
-        for x in range(0,9):
-                for y in range(0,9):
-                        if grid[x][y] == 0:
-                                return x,y
-        return -1,-1
-
-def isValid(grid, i, j, e):
-        rowOk = all([e != grid[i][x] for x in range(9)])
-        if rowOk:
-                columnOk = all([e != grid[x][j] for x in range(9)])
-                if columnOk:
-                        # finding the top left x,y co-ordinates of the section containing the i,j cell
-                        secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
-                        for x in range(secTopX, secTopX+3):
-                                for y in range(secTopY, secTopY+3):
-                                        if grid[x][y] == e:
-                                                return False
-                        return True
+    def _validInput(self, grid):
+        # Grid type should be 9x9 only considering list of lists for now
+        if isinstance(grid, list):
+            if len(grid) == 9:
+                if (False in [len(x) == 9 for x in grid]) == False:
+                    return True
         return False
-
-def solveSudoku(grid, i=0, j=0):
+            
     
-    # Only solve valid boards
-    if validInput(grid):
+    def _findNextCellToFill(self, grid, i, j):
+            for x in range(i,9):
+                    for y in range(j,9):
+                            if grid[x][y] == 0:
+                                    return x,y
+            for x in range(0,9):
+                    for y in range(0,9):
+                            if grid[x][y] == 0:
+                                    return x,y
+            return -1,-1
     
-        i,j = findNextCellToFill(grid, i, j)
-        if i == -1:
-                return True
-        for e in range(1,10):
-                if isValid(grid,i,j,e):
-                        grid[i][j] = e
-                        if solveSudoku(grid, i, j):
-                                return grid
-                        # Undo the current cell for backtracking
-                        grid[i][j] = 0
-    return False
+    def _isValid(self, grid, i, j, e):
+            rowOk = all([e != grid[i][x] for x in range(9)])
+            if rowOk:
+                    columnOk = all([e != grid[x][j] for x in range(9)])
+                    if columnOk:
+                            # finding the top left x,y co-ordinates of the section containing the i,j cell
+                            secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here. 
+                            for x in range(secTopX, secTopX+3):
+                                    for y in range(secTopY, secTopY+3):
+                                            if grid[x][y] == e:
+                                                    return False
+                            return True
+            return False
+    
+    def solveSudoku(self, grid, i=0, j=0):
+        
+        # Only solve valid boards
+        if self._validInput(grid):
+        
+            i,j = self._findNextCellToFill(grid, i, j)
+            if i == -1:
+                    return True
+            for e in range(1,10):
+                    if self._isValid(grid,i,j,e):
+                            grid[i][j] = e
+                            if self.solveSudoku(grid, i, j):
+                                    return grid
+                            # Undo the current cell for backtracking
+                            grid[i][j] = 0
+        return False
     
 # TODO: Create medium and hard sudoku puzzles
 # TODO: Create unsolvable puzzle
