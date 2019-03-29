5d7c877c12dd730838a950a31edae95b5ec98d37
Matt Gray <blampz@hotmail.com>
2019-03-29 14:25:06 -0400

    Started work on unit/integration tests

--- sudokusolver/tests/test_detection.py
+++ sudokusolver/tests/test_detection.py
@@ -7,43 +7,38 @@ Created on Tue Mar 26 09:30:50 2019
 """
 
 import os, sys, unittest
-#sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('sudokusolver'))
 
-#from detection import SudokuDetector
+from detection_class import SudokuDetector
 
 # XMLrunner used to tie into Jenkins unit testing
 import xmlrunner
-
-# REPLACE THIS STUB WITH ACTUAL IMPORT WHEN CLASS IS WRITTEN
-class SudokuDetector():
-    def detectSudoku(self, fpath):
-        return 0
 
 class TestSudokuDetection(unittest.TestCase):
     
     def setUp(self):
         self.sudokuDetector = SudokuDetector()
     
-    def test_newspaper_image(self):
-        
-        # Test image taken from mobile camera on newspaper
-        
-        input_filepath = 'tests/newspaper_image.jpg'
-        
-        output_case = [[0, 0, 0, 6, 0, 4, 7, 0, 0],
-                       [7, 0, 6, 0, 0, 0, 0, 0, 9],
-                       [0, 0, 0, 0, 0, 5, 0, 8, 0],
-                       [0, 7, 0, 0, 2, 0, 0, 9, 3],
-                       [8, 0, 0, 0, 0, 0, 0, 0, 5],
-                       [4, 3, 0, 0, 1, 0, 0, 7, 0],
-                       [0, 5, 0, 2, 0, 0, 0, 0, 0],
-                       [3, 0, 0, 0, 0, 0, 2, 0, 8],
-                       [0, 0, 2, 3, 0, 1, 0, 0, 0]]
-        
-        output_result = self.sudokuDetector.detectSudoku(input_filepath)
-        
-        self.assertEqual(output_result, output_case)
+#    def test_newspaper_image(self):
+#        
+#        # Test image taken from mobile camera on newspaper
+#        
+#        input_filepath = 'tests/newspaper_image.jpg'
+#        
+#        output_case = [[0, 0, 0, 6, 0, 4, 7, 0, 0],
+#                       [7, 0, 6, 0, 0, 0, 0, 0, 9],
+#                       [0, 0, 0, 0, 0, 5, 0, 8, 0],
+#                       [0, 7, 0, 0, 2, 0, 0, 9, 3],
+#                       [8, 0, 0, 0, 0, 0, 0, 0, 5],
+#                       [4, 3, 0, 0, 1, 0, 0, 7, 0],
+#                       [0, 5, 0, 2, 0, 0, 0, 0, 0],
+#                       [3, 0, 0, 0, 0, 0, 2, 0, 8],
+#                       [0, 0, 2, 3, 0, 1, 0, 0, 0]]
+#        
+#        output_result = self.sudokuDetector.detectSudoku(input_filepath)
+#        
+#        self.assertEqual(output_result, output_case)
         
     def test_ideal_image(self):
         
@@ -61,29 +56,29 @@ class TestSudokuDetection(unittest.TestCase):
                        [0, 5, 1, 0, 0, 2, 0, 0, 0],
                        [9, 0, 0, 0, 5, 0, 8, 7, 0]]
         
-        output_result = self.sudokuDetector.detectSudoku(input_filepath)
+        output_result = self.sudokuDetector.detect_board(input_filepath)
         
         self.assertEqual(output_result, output_case)
         
-    def test_image_with_background_colours(self):
-        
-        # Test online screenshot containing background colours
-        
-        input_filepath = 'tests/image_with_background_colours.png'
-        
-        output_case = [[0, 0, 0, 4, 0, 0, 0, 8, 0],
-                       [1, 9, 0, 6, 0, 0, 4, 5, 0],
-                       [0, 2, 0, 0, 8, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 0, 0, 0, 9, 7],
-                       [0, 0, 2, 0, 0, 0, 6, 0, 0],
-                       [8, 1, 0, 0, 0, 0, 0, 0, 0],
-                       [0, 0, 0, 0, 7, 0, 0, 6, 0],
-                       [0, 7, 3, 0, 0, 5, 0, 1, 9],
-                       [0, 4, 0, 0, 0, 9, 0, 0, 0]]
-        
-        output_result = self.sudokuDetector.detectSudoku(input_filepath)
-        
-        self.assertEqual(output_result, output_case)
+#    def test_image_with_background_colours(self):
+#        
+#        # Test online screenshot containing background colours
+#        
+#        input_filepath = 'tests/image_with_background_colours.png'
+#        
+#        output_case = [[0, 0, 0, 4, 0, 0, 0, 8, 0],
+#                       [1, 9, 0, 6, 0, 0, 4, 5, 0],
+#                       [0, 2, 0, 0, 8, 0, 0, 0, 0],
+#                       [0, 0, 0, 0, 0, 0, 0, 9, 7],
+#                       [0, 0, 2, 0, 0, 0, 6, 0, 0],
+#                       [8, 1, 0, 0, 0, 0, 0, 0, 0],
+#                       [0, 0, 0, 0, 7, 0, 0, 6, 0],
+#                       [0, 7, 3, 0, 0, 5, 0, 1, 9],
+#                       [0, 4, 0, 0, 0, 9, 0, 0, 0]]
+#        
+#        output_result = self.sudokuDetector.detectSudoku(input_filepath)
+#        
+#        self.assertEqual(output_result, output_case)
    
 
 if __name__ == '__main__':
